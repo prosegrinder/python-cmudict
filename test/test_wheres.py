f18bb1a2095914dd57a87edc4596e0b3c2f8c52b
David L. Day <dday376@gmail.com>
2018-02-11 00:02:33 -0500

    Simplified test_wheres and explicitly raise assertionerror.

--- test/test_wheres.py
+++ test/test_wheres.py
@@ -3,17 +3,21 @@
 import cmudict
 from os import path
 
+def _assert_filesize(filename):
+    if (0 >= path.getsize(filename)):
+        raise AssertionError(filename + " <= 0 bytes")
+
 def test_where():
-    assert path.isfile(cmudict.where())
+    _assert_filesize(cmudict.where())
 
 def test_where_license():
-    assert path.isfile(cmudict.where_license())
+    _assert_filesize(cmudict.where_license())
 
 def test_where_phones():
-    assert path.isfile(cmudict.where_phones())
+    _assert_filesize(cmudict.where_phones())
 
 def test_where_symbols():
-    assert path.isfile(cmudict.where_symbols())
+    _assert_filesize(cmudict.where_symbols())
 
 def test_where_vp():
-    assert path.isfile(cmudict.where_vp())
+    _assert_filesize(cmudict.where_vp())
