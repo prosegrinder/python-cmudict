b877da07f171fdc3b2cb0a22ce33f3ba2191cb4a
David L. Day <dday376@gmail.com>
2018-02-12 22:36:28 -0500

    Use pkg_resources instead of os and added stream functions for data files.

--- cmudict/__init__.py
+++ cmudict/__init__.py
@@ -1,53 +1,83 @@
 # -*- coding: utf-8 -*-
 
 """
-This module returns the installation location of various cmudict files.
+This module provides access to the location and stream of the cmudict files.
 The default function where() returns the location of the cmudict.dict file.
 """
-
-import warnings
-from os import path
 
 import pkg_resources
 
 __version__ = pkg_resources.resource_string('cmudict', 'VERSION').strip()
 
-def _where(filename):
-    f = pkg_resources.resource_filename(__name__, filename)
+
+def _where(resource_name):
+    f = pkg_resources.resource_filename(__name__, resource_name)
     return f
+
+
+def _stream(resource_name):
+    s = pkg_resources.resource_stream(__name__, resource_name)
+    return s
+
 
 def where():
     """The location of the cmudict.dict file."""
-    f = pkg_resources.resource_filename(__name__, 'data/cmudict.dict')
+    f = _where('data/cmudict.dict')
     return f
+
 
 def stream():
     """A readable file-like object of the cmudict.dict file."""
-    s = pkg_resources.resource_stream(__name__, 'data/cmudict.dict')
+    s = _stream('data/cmudict.dict')
     return s
+
 
 def where_license():
     """The location of the cmudict LICENSE file."""
-    f = pkg_resources.resource_filename(__name__, 'data/LICENSE')
+    f = _where('data/LICENSE')
     return f
+
+
+def stream_license():
+    """A readable file-like object of the LICENSE file."""
+    s = _stream('data/LICENSE')
+    return s
 
 
 def where_phones():
     """The location of the cmudict.phones file."""
-    f = pkg_resources.resource_filename(__name__, 'data/cmudict.phones')
+    f = _where('data/cmudict.phones')
     return f
+
+
+def stream_phones():
+    """A readable file-like object of the cmudict.phones file."""
+    s = _stream('data/cmudict.phones')
+    return s
 
 
 def where_symbols():
     """The location of the cmudict.symbols file."""
-    f = pkg_resources.resource_filename(__name__, 'data/cmudict.symbols')
+    f = _where('data/cmudict.symbols')
     return f
+
+
+def stream_symbols():
+    """A readable file-like object of the cmudict.symbols file."""
+    s = _stream('data/cmudict.symbols')
+    return s
 
 
 def where_vp():
     """The location of the cmudict.vp file."""
-    f = pkg_resources.resource_filename(__name__, 'data/cmudict.vp')
+    f = _where('data/cmudict.vp')
     return f
+
+
+def stream_vp():
+    """A readable file-like object of the cmudict.vp file."""
+    s = _stream('data/cmudict.vp')
+    return s
 
 
 if __name__ == '__main__':
