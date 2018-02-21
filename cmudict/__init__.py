8bc7003c06413938a6eb61ac1397fc89589fabfd
David L. Day <dday376@gmail.com>
2018-02-20 22:51:47 -0500

    Ditching wheres in favor of streams and strings.

--- cmudict/__init__.py
+++ cmudict/__init__.py
@@ -14,74 +14,23 @@ __version__ = pkg_resources.resource_string(
     'cmudict', 'VERSION').decode('utf-8').strip()
 
 
-def _where(resource_name):
-    f = pkg_resources.resource_filename(__name__, resource_name)
-    return f
-
-
 def _stream(resource_name):
-    s = pkg_resources.resource_stream(__name__, resource_name)
-    return s
+    stream = pkg_resources.resource_stream(__name__, resource_name)
+    return stream
 
 
-def where():
-    """The location of the cmudict.dict file."""
-    f = _where('data/cmudict.dict')
-    return f
+def _string(resource_name):
+    string = pkg_resources.resource_string(__name__, resource_name)
+    return string
 
 
-def stream():
-    """A readable file-like object of the cmudict.dict file."""
-    s = _stream('data/cmudict.dict')
-    return s
-
-
-def where_license():
-    """The location of the cmudict LICENSE file."""
-    f = _where('data/LICENSE')
-    return f
-
-
-def stream_license():
-    """A readable file-like object of the LICENSE file."""
-    s = _stream('data/LICENSE')
-    return s
-
-
-def where_phones():
-    """The location of the cmudict.phones file."""
-    f = _where('data/cmudict.phones')
-    return f
-
-
-def stream_phones():
-    """A readable file-like object of the cmudict.phones file."""
-    s = _stream('data/cmudict.phones')
-    return s
-
-
-def where_symbols():
-    """The location of the cmudict.symbols file."""
-    f = _where('data/cmudict.symbols')
-    return f
-
-
-def stream_symbols():
-    """A readable file-like object of the cmudict.symbols file."""
-    s = _stream('data/cmudict.symbols')
-    return s
-
-
-def where_vp():
-    """The location of the cmudict.vp file."""
-    f = _where('data/cmudict.vp')
-    return f
-
-
-def stream_vp():
-    """A readable file-like object of the cmudict.vp file."""
-    s = _stream('data/cmudict.vp')
-    return s
+def _entries(stream):
+    entries = []
+    for line in stream:
+        parts = line.decode('utf-8').strip().split()
+        thing = re.sub(r'\(\d+\)$', '', parts[0])
+        entries.append((thing, parts[1:]))
+    return entries
 
 
 def dict():
@@ -96,6 +45,83 @@ def dict():
     return default
 
 
+def dict_stream():
+    """Return a readable file-like object of the cmudict.dict file."""
+    stream = _stream('data/cmudict.dict')
+    return stream
+
+
+def dict_string():
+    """Return the contents of cmudict.dict as a string."""
+    string = _string('data/cmudict.dict')
+    return string
+
+
+def license_string():
+    """Return the contents of LICENSE as a string."""
+    string = _string('data/LICENSE')
+    return string
+
+
+def phones():
+    phones = []
+    for line in phones_stream():
+        parts = line.decode('utf-8').strip().split()
+        phones.append((parts[0], parts[1:]))
+    return phones
+
+
+def phones_stream():
+    """Return a readable file-like object of the cmudict.phones file."""
+    s = _stream('data/cmudict.phones')
+    return s
+
+
+def phones_string():
+    """Return the contents of cmudict.phones as a string."""
+    string = _string('data/cmudict.phones')
+    return string
+
+
+def symbols():
+    symbols = []
+    for line in symbols_stream():
+        symbols.append(line.decode('utf-8').strip())
+    return symbols
+
+
+def symbols_stream():
+    """Return a readable file-like object of the cmudict.symbols file."""
+    stream = _stream('data/cmudict.symbols')
+    return stream
+
+
+def symbols_string():
+    """Return the contents of cmudict.symbols as a string."""
+    string = _string('data/cmudict.symbols')
+    return string
+
+
+def vp():
+    vp = defaultdict(list)
+    for key, value in _entries(vp_stream()):
+        vp[key].append(value)
+    return vp
+
+
+def vp_stream():
+    """Return a readable file-like object of the cmudict.vp file."""
+    stream = _stream('data/cmudict.vp')
+    return stream
+
+
+def vp_string():
+    """Return the contents of cmudict.vp as a string."""
+    string = _stream('data/cmudict.vp')
+    return string
+
+
+# The entries(), raw(), and words() maintain compatability with NTLK
 def entries():
     """
     Compatibility with NLTK.
@@ -103,7 +129,7 @@ def entries():
     containing (word, transcriptions) tuples.
     """
     entries = []
-    for line in stream():
+    for line in dict_stream():
         parts = line.decode('utf-8').strip().split()
         word = re.sub(r'\(\d+\)$', '', parts[0])
         entries.append((word, parts[1:]))
@@ -125,7 +151,3 @@ def words():
     Returns a list of all words defined in the cmudict lexicon.
     """
     return [word.lower() for (word, _) in entries()]
-
-
-if __name__ == '__main__':
-    print(where())
