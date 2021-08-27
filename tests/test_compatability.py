940fa5562c1344aec8198ee86e7ad3607e833a3c
David L. Day <1132144+davidlday@users.noreply.github.com>
2021-08-27 07:12:21 -0400

    Maintenance updates 2021 08 (#22)
    * added test publish
    
    * use build and publish gh action
    
    * added lint and check_build
    
    * added missing configs
    
    * added pyproject.toml
    
    * moved code to src
    
    * formatted and linted
    
    * updated package specs
    
    * README.rst -> README.md
    
    * v1.0.1
    
    * moved code to src
    
    * updated module description
    
    * checkout with submodules
    
    * formatted with black
    
    * fixed line length

--- tests/test_compatability.py
+++ tests/test_compatability.py
@@ -7,33 +7,37 @@ def test_dict():
     EXPECTED_SIZE = 126046
     d = cmudict.dict()
     SIZE = len(d)
-    if (EXPECTED_SIZE != SIZE):
-        raise AssertionError('cmudict.dict(): Expected {0} keys, got {1}.'.format(
-            EXPECTED_SIZE, SIZE))
+    if EXPECTED_SIZE != SIZE:
+        raise AssertionError(
+            "cmudict.dict(): Expected {0} keys, got {1}.".format(EXPECTED_SIZE, SIZE)
+        )
 
 
 def test_entries():
     EXPECTED = 135155
     e = cmudict.entries()
     COUNT = len(e)
-    if (COUNT != EXPECTED):
-        raise AssertionError('cmudict.entries(): Expected {0} entries, got {1}.'.format(
-            EXPECTED, COUNT))
+    if COUNT != EXPECTED:
+        raise AssertionError(
+            "cmudict.entries(): Expected {0} entries, got {1}.".format(EXPECTED, COUNT)
+        )
 
 
 def test_raw():
     EXPECTED = 3618090
     r = cmudict.raw()
     COUNT = len(r)
-    if (COUNT != EXPECTED):
-        raise AssertionError('cmudict.raw(): Expected {0} bytes, got {1}.'.format(
-            EXPECTED, COUNT))
+    if COUNT != EXPECTED:
+        raise AssertionError(
+            "cmudict.raw(): Expected {0} bytes, got {1}.".format(EXPECTED, COUNT)
+        )
 
 
 def test_words():
     EXPECTED = 135155
     w = cmudict.words()
     COUNT = len(w)
-    if (COUNT != EXPECTED):
-        raise AssertionError('cmudict.raw(): Expected {0} bytes, got {1}.'.format(
-            EXPECTED, COUNT))
+    if COUNT != EXPECTED:
+        raise AssertionError(
+            "cmudict.raw(): Expected {0} bytes, got {1}.".format(EXPECTED, COUNT)
+        )
