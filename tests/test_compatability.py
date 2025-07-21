f13d830066f0e73bae310233d72abc7cc340a497
David L. Day <dday376@gmail.com>
2025-07-21 06:23:51 -0400

    test: bring compatibility tests up to current standards

--- tests/test_compatability.py
+++ tests/test_compatability.py
@@ -1,43 +1,42 @@
-# -*- coding: utf-8 -*-
-
+# pylint: disable=missing-docstring
 import cmudict
 
 
 def test_dict():
-    EXPECTED_SIZE = 126052
+    expected_size = 126052
     d = cmudict.dict()
-    SIZE = len(d)
-    if EXPECTED_SIZE != SIZE:
+    actual_size = len(d)
+    if expected_size != actual_size:
         raise AssertionError(
-            "cmudict.dict(): Expected {0} keys, got {1}.".format(EXPECTED_SIZE, SIZE)
+            f"cmudict.dict(): Expected {expected_size} keys, got {actual_size}."
         )
 
 
 def test_entries():
-    EXPECTED = 135166
+    expected_count = 135166
     e = cmudict.entries()
-    COUNT = len(e)
-    if COUNT != EXPECTED:
+    actual_count = len(e)
+    if actual_count != expected_count:
         raise AssertionError(
-            "cmudict.entries(): Expected {0} entries, got {1}.".format(EXPECTED, COUNT)
+            f"cmudict.entries(): Expected {expected_count} entries, got {actual_count}."
         )
 
 
 def test_raw():
-    EXPECTED = 3618488
+    expected_bytes = 3618488
     r = cmudict.raw()
-    COUNT = len(r)
-    if COUNT != EXPECTED:
+    actual_bytes = len(r)
+    if actual_bytes != expected_bytes:
         raise AssertionError(
-            "cmudict.raw(): Expected {0} bytes, got {1}.".format(EXPECTED, COUNT)
+            f"cmudict.raw(): Expected {expected_bytes} bytes, got {actual_bytes}."
         )
 
 
 def test_words():
-    EXPECTED = 135166
+    expected_count = 135166
     w = cmudict.words()
-    COUNT = len(w)
-    if COUNT != EXPECTED:
+    actual_count = len(w)
+    if actual_count != expected_count:
         raise AssertionError(
-            "cmudict.raw(): Expected {0} bytes, got {1}.".format(EXPECTED, COUNT)
+            f"cmudict.words(): Expected {expected_count} words, got {actual_count}."
         )
